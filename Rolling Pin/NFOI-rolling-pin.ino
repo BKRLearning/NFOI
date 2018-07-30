ef73b37b406fcd60203e1422a5f5e3f1c199675d
Nick Wallace <ntwallace@gmail.com>
2018-07-30 14:37:46 -0400

    added 2nd blender, updated delay structure

--- Rolling Pin/NFOI-rolling-pin.ino
+++ Rolling Pin/NFOI-rolling-pin.ino
@@ -1,28 +1,34 @@
 #include <Encoder.h>
 #include <Bounce.h>
 
-const int buttonPin = 12;
+#define BUTTON_PIN 15
+#define FOOD_PROCESSOR_PIN 14
+#define BLENDER_PIN 13
 
-Bounce pushbutton = Bounce(buttonPin, 5);  // 5ms debounce
+Bounce pushbutton = Bounce(BUTTON_PIN, 15);  // 5ms debounce
 byte buttonState, previousState;
 
 Encoder knob(6, 7);
 long currentPos, secondPos;
 
+const long releaseDelay = 40;  // delay in ms before button releases
+
 long startMillis;
 long currentMillis;
-const long period = 10; // time length (ms)
+const long period = 10; // delay period in ms between analog reads
 
 void setup() {
   Serial.begin(9600);
   startMillis = millis();
 
-  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(FOOD_PROCESSOR_PIN, OUTPUT);
+  pinMode(BLENDER_PIN, OUTPUT);
 
   currentPos = knob.read();
   secondPos = currentPos - 1;
 
-  buttonState = digitalRead(buttonPin);
+  buttonState = digitalRead(BUTTON_PIN);
   previousState = HIGH;
 }
 
@@ -38,8 +44,15 @@ void loop() {
 
   if (pushbutton.fallingEdge()) {    // button was pressed
     Serial.println("button pressed");
+
+    digitalWrite(FOOD_PROCESSOR_PIN, HIGH);
+    digitalWrite(BLENDER_PIN, HIGH);
     Keyboard.press(KEY_A);
-    delay(20);
+    
+    delay(releaseDelay);  // delay needs to be long enough that keypress is registered by computer & mechanical devices are powered on long enough for 
+    
+    digitalWrite(FOOD_PROCESSOR_PIN, LOW);
+    digitalWrite(BLENDER_PIN, LOW);
     Keyboard.release(KEY_A);
   }
 
@@ -54,7 +67,7 @@ void loop() {
     secondPos = currentPos;
 
   } else {
-    delay(100);
+    delay(releaseDelay);
     Keyboard.release(KEY_LEFT);
     Keyboard.release(KEY_RIGHT);
   }
