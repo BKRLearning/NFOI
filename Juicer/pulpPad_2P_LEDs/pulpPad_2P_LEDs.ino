9aff7852cae0b262db8e61bf89b900b0ab1e24d0
Nick Wallace <ntwallace@gmail.com>
2018-08-14 16:09:04 -0400

    Updated enter button code, added additional comments

--- Juicer/pulpPad_2P_LEDs/pulpPad_2P_LEDs.ino
+++ Juicer/pulpPad_2P_LEDs/pulpPad_2P_LEDs.ino
@@ -1,16 +1,38 @@
+/****************************************
+
+  Pulp Pad Controller
+
+  Description text to go here...
+  ...
+  ...
+
+  Created August 14, 2018
+  By Nick Wallace <ntwallace@gmail.com> for Brooklyn Research: New Forms of Interaction
+  http://www.github.com/BKRLearning/NFOI/blob/master/Juicer/current/
+
+  This code is free software under the MIT liscense, all text above must be included in any redistribution.
+
+****************************************/
+
+
+//
+// Define input and output pins, default is 2p, 4 juicers + 1 enter button per player 
+//
+
+#include <Bounce.h>
+
 // define input pins for juicers
-#define JUICER_P1_LEFT 18
-#define JUICER_P1_RIGHT 17
-#define JUICER_P1_UP 16
-#define JUICER_P1_DOWN 15
-#define JUICER_P1_START 14
+#define JUICER_P1_LEFT 23
+#define JUICER_P1_RIGHT 22
+#define JUICER_P1_UP 21
+#define JUICER_P1_DOWN 20
+#define JUICER_P1_ENTER 19
 
-#define JUICER_P2_LEFT 13
-#define JUICER_P2_RIGHT 12
-#define JUICER_P2_UP 11
-#define JUICER_P2_DOWN 10
-#define JUICER_P2_START 9
-
+#define JUICER_P2_LEFT 18
+#define JUICER_P2_RIGHT 17
+#define JUICER_P2_UP 16
+#define JUICER_P2_DOWN 15
+#define JUICER_P2_ENTER 14
 
 // define output pins for LEDs
 #define LED_P1_LEFT 4
@@ -22,7 +44,6 @@
 #define LED_P2_RIGHT 9
 #define LED_P2_UP 10
 #define LED_P2_DOWN 11
-
 
 // define juicer key presses
 #define p1Left KEY_LEFT
@@ -37,22 +58,41 @@
 #define p2Down KEY_S
 #define p2Start KEY_ENTER
 
+// setup enter buttons, 1 per player
+Bounce enterP1 = Bounce(JUICER_P1_ENTER, 5);  // 5ms debounce
+Bounce enterP2 = Bounce(JUICER_P2_ENTER, 5);  // 5ms debounce
 
-#define totalJuicers 8    // 4 juicers per player times 2 players
-uint8_t inputPins[totalJuicers] = {JUICER_P1_LEFT, JUICER_P1_RIGHT, JUICER_P1_UP, JUICER_P1_DOWN, JUICER_P2_LEFT, JUICER_P2_RIGHT, JUICER_P2_UP, JUICER_P2_DOWN};
+
+
+//
+// Setup Game Variables
+//
+
+#define totalPlayers 2
+#define totalJuicers (totalPlayers * 4)    // 4 juicers per player
+uint8_t juicerInputs[totalJuicers] = {JUICER_P1_LEFT, JUICER_P1_RIGHT, JUICER_P1_UP, JUICER_P1_DOWN, JUICER_P2_LEFT, JUICER_P2_RIGHT, JUICER_P2_UP, JUICER_P2_DOWN};
 uint8_t ledOutputs[totalJuicers] = {LED_P1_LEFT, LED_P1_RIGHT, LED_P1_UP, LED_P1_DOWN, LED_P2_LEFT, LED_P2_RIGHT, LED_P2_UP, LED_P2_DOWN};
 uint8_t outputKeys[totalJuicers] = {p1Left, p1Right, p1Up, p1Down, p2Left, p2Right, p2Up, p2Down};
 
-// arrays for juicer pressed/released bools to be in order L, R, U, D for p1 & p2
+uint8_t enterInputs[totalPlayers] = {JUICER_P1_ENTER, JUICER_P2_ENTER};
+Bounce enterButtons[totalPlayers] = {enterP1, enterP2};
+
+
+
+//
+// Setup Arrays for Gameplay
+//
+
+// juicer pressed/released bools, to be in order L, R, U, D for p1 & p2
 byte juicersPressed[totalJuicers];
 byte juicersReleased[totalJuicers];
 
-// arrays for juicer analog readings to be in order L, R, U, D for p1 & p2
+// juicer analog readings, to be in order L, R, U, D for p1 & p2
 uint16_t baselineVals[totalJuicers];
 uint16_t currentVals[totalJuicers];
 uint16_t previousVals[totalJuicers];
 
-// arrays for pressed juicer running avg values
+// pressed juicer running avg values
 const byte arraySize = 6;
 long pushedVals[totalJuicers][arraySize];    // 2D array contains array for each juicer with the last "arraySize" readings of each juicer to use for averaging
 byte readIndexes[totalJuicers];
@@ -65,6 +105,7 @@ long currentMillis;
 const long period = 7;
 
 
+
 void setup() {
   Serial.begin(9600);
 
@@ -74,13 +115,14 @@ void setup() {
 }
 
 void loop() {
+  getEnterButtons(); // check if enters buttons have been pressed
 
   currentMillis = millis();
   if (currentMillis - startMillis >= period) {  // run this code every "period" (in ms)
 
     getVals(); // get current vals for each juicer
 
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < totalJuicers; i++) {
       if (juicersPressed[i]) {  // if a particular juicer was pressed, then...
         totals[i] = totals[i] - pushedVals[i][readIndexes[i]];  // subtract the last value from the running total
         pushedVals[i][readIndexes[i]] = currentVals[i]; // get the new pressed value from the current value array
@@ -114,7 +156,7 @@ void loop() {
 
 void initJuicers() {  // get juicer baseline readings & initialize arrays and LEDs
   for (int i = 0; i < totalJuicers; i++) {
-    baselineVals[i] = analogRead(inputPins[i]);
+    baselineVals[i] = analogRead(juicerInputs[i]);
 
     pinMode(ledOutputs[i], OUTPUT);
 
@@ -134,11 +176,23 @@ void initJuicers() {  // get juicer baseline readings & initialize arrays and LE
   }
 }
 
+void getEnterButtons() {
+  for (int i = 0; i < totalPlayers; i++) {
+    enterButtons[i].update();
+
+    if (enterButtons[i].fallingEdge()) {
+      Keyboard.press(JUICER_P1_ENTER);
+      delay(20);  // has to be long enough that keypress is registered by computer, min 20ms
+      Keyboard.release(JUICER_P1_ENTER);
+    }
+  }
+}
+
 
 void getVals() {  // get current values for each juicer
   for (int i = 0; i < totalJuicers; i++) {
     previousVals[i] = currentVals[i];
-    currentVals[i] = analogRead(inputPins[i]);
+    currentVals[i] = analogRead(juicerInputs[i]);
   }
 
   delay(1);
